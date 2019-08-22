64ad496138fff842fdfd758ac3eb2e23e1bfcfea
Jiayi Wang <clairejw1015@gmail.com>
2019-08-22 00:48:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    
+    n = array.find_index(cheese_types)
+  else
+    return nil
   
 end
