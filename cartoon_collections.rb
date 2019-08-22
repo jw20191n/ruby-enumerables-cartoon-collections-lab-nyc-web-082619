5026500547f9f1cdab636f713a8d744f31c2229a
Jiayi Wang <clairejw1015@gmail.com>
2019-08-22 00:35:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  if array.length > 4 
+    return true
+  else
+    return false
+  end
 end
 
 def find_the_cheese# code an argument here
